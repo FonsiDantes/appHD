67f52926c88c08d0cdaae0c2edb58fb01c7eb661
Fonsi <davidarb.darb@gmail.com>
2023-06-13 22:42:35 -0600

    cuarto commit

--- appHD/src/app/login/login.page.ts
+++ appHD/src/app/login/login.page.ts
@@ -40,6 +40,24 @@ export class LoginPage implements OnInit {
       this.form.markAllAsTouched();
     }
   }
-
 }
- 
+
+
+
+//login(){
+//  const email = this.loginUsuario.value.email;
+//  const password = this.loginUsuario.value.password;
+//
+//  this.loading = true;
+//  this.afAuth.signInWithEmailAndPassword(email, password).then((user) =>{
+//    if(user.user?.emailVerified){
+//      this.router.navigate(['/dashboard']);
+//    }
+//    else{
+//      this.router.navigate(['/verificar-correo']);
+//    }
+//  }).catch((error) =>{
+//    this.loading=false;
+//    this.firebaseError.codeError(error.code);  
+//  })
+//}
