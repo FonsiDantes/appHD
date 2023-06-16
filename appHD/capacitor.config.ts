5b6e7a34ac4977e08f3664bc3cb3df9764c5cd1c
Fonsi <davidarb.darb@gmail.com>
2023-06-16 13:25:50 -0600

    sexto commit

--- appHD/capacitor.config.ts
+++ appHD/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'app20',
+  appName: 'Help Desk',
   webDir: 'www',
 };
 
